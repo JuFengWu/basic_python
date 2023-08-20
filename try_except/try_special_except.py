8e8a929d7ed9d410cde4856b0b382a21fc7d4a66
leo <leowu102000@gmail.com>
2023-08-20 23:13:51 +0800

    add except

--- try_except/try_special_except.py
+++ try_except/try_special_except.py
@@ -1,7 +1,11 @@
 try:
-    print(3/0)
+    #print(3/0)
+    a=3
+    c = a/b
 except ZeroDivisionError:
     # 捕捉除以零的例外
     print("不能除以零!")
+except Exception:
+    print("總之有個錯誤")
 
 print("do something")   
